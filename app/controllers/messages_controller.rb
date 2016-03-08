ffeb6e3a8167932c1567f5c94f8f2ca165053177
Lauren Caponong <lcaponong@gmail.com>
2016-03-08 13:53:47 -0500

    Setting up comments...

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,16 +7,14 @@ class MessagesController < ApplicationController
  	end
 
  	def show
- 		@message = Message.find(params[:id])
  	end 
 
-
  	def new
- 		@message = Message.new
+ 		@message = current_user.messages.build
  	end
 
  	def create
- 		@message = Message.new(message_params)
+ 		@message = current_user.messages.build(message_params)
  		if @message.save
  			redirect_to root_path
  		else
